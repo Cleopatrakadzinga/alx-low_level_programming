d362e39f4d37e299133c49799e16ec21e3a18950
Cleopatrakadzinga <cleopatrakadzingagmail.com>
2022-12-11 15:34:21 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@
  */
 
 int main(void)
-
 {
-		char c;
-			int n;
-				for (n = '0'; n <= '9'; n++)
-							putchar(n);
-					for (c = 'a'; c <= 'f'; c++)
-								putchar(c);
-						putchar('\n');
-							return (0);
+	char c;
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
