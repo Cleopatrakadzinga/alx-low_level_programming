b2e3d5c43aff2427a4338847d5616022f6b78942
Cleopatrakadzinga <cleopatrakadzingagmail.com>
2022-12-11 16:04:24 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,34 +9,34 @@
  */
 
 int main(void)
-
 {
-		int i = 0;
-			int j, k;
-				int count = 0;
-					while (i <= 7)
-							{
-										j = i + 1;
-												while (j <= 8)
-															{
-																			k = j + 1;
-																						while (k <= 9)
-																										{
-																															putchar(i + '0');
-																																			putchar(j + '0');
-																																							putchar(k + '0');
-																																											if (count != 119)
-																																																{
-																																																						putchar(',');
-																																																											putchar(' ');
-																																																															}
-																																															k++;
-																																																			count++;
-																																																						}
-																									j++;
-																											}
-														i++;
-															}
-						putchar('\n');
-							return (0);
+	int i = 0;
+	int j, k;
+	int count = 0;
+
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (count != 119)
+				{
+					putchar(',');
+					putchar(' ');
+					}
+				k++;
+				count++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n')
+	return (0);
 }
