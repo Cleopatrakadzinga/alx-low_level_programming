2937907fe5a54cbce54d24a809f3bdadfd581d73
Cleopatrakadzinga <cleopatrakadzingagmail.com>
2022-12-16 16:18:09 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int number;
+	{
+
+	for (number = '0'; number <= '9'; number++)
+		;
+	else
+		_putchar(number);
+	}
+	_putchar('\n');
+}
