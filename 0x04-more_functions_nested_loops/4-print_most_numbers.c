12b21cbb0ba337fc80ce2650faf46f735e4d6796
Cleopatrakadzinga <cleopatrakadzingagmail.com>
2022-12-16 16:34:47 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print numbers except 2 and 4
+ * print_most_numbers - entry point
  *
  * Description: Prints the numbers with _putchar
  *
@@ -13,13 +13,12 @@ void print_most_numbers(void)
 	int number;
 	{
 
-	for (number = 48; number < 58; number++)
+	for (number = '0'; number < '9'; number++)
 	{
-		if ((number == 50) || (number == 52))
-		{
-			continue;
-		}
-		_putchar(number);
+		if ((number == '2') || (number == '4'))
+			;
+		else
+			_putchar(number);
 	}
-	_putchar(10);
+	_putchar('\n');
 }
