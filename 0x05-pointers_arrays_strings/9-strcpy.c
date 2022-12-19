bd4922fbed4c9540b8113e50aa344ae16e734351
Cleopatrakadzinga <cleopatrakadzingagmail.com>
2022-12-19 06:29:52 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,18 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int len = 0;
+	int l = 0;
+	int x = 0;
 
-	while (*(src + len) != '\0')
-		{
-			*(dest + len) * *(src + len);
-			len++;
-		}
-	*(dest + len) = '\0';
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for (x = 0; x < l; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+
+	return (dest);
 }
