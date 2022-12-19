c2eb6c0449c9baff949156ae5cc6262536600e6d
Cleopatrakadzinga <cleopatrakadzingagmail.com>
2022-12-19 02:26:54 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * rev_string - Reverse a string 
+ * rev_string - Reverse a string
  * @s: Input string
- * Return: String in revere
- **/
+ * Return: String in reverse
+ */
 
 void rev_string(char *s)
 {
